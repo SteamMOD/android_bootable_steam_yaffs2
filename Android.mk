0d2f30f28dc2fd3d89f79fe3d9b0a922ea35cef8
Zsolt Sz. Sztupák <mail@sztupy.hu>
2010-12-20 18:49:59 -0500

    changed to libsteam standards

--- Android.mk
+++ Android.mk
@@ -9,13 +9,13 @@ LOCAL_SRC_FILES := \
 	yaffs2/yaffs_ecc.c \
 	yaffs2/yaffs_tagsvalidity.c
 
-LOCAL_CFLAGS =   -O2 -Wall -DCONFIG_YAFFS_UTIL -DCONFIG_YAFFS_DOES_ECC
+LOCAL_CFLAGS =   -Os -Wall -DCONFIG_YAFFS_UTIL -DCONFIG_YAFFS_DOES_ECC
 LOCAL_CFLAGS+=   -Wshadow -Wpointer-arith -Wwrite-strings -Wstrict-prototypes -Wmissing-declarations
 LOCAL_CFLAGS+=   -Wmissing-prototypes -Wredundant-decls -Wnested-externs -Winline
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/yaffs2
 
-LOCAL_MODULE := mkyaffs2image
+LOCAL_MODULE := steam_mkyaffs2image
 
 include $(BUILD_HOST_EXECUTABLE)
 
@@ -28,12 +28,12 @@ LOCAL_SRC_FILES := \
 	yaffs2/yaffs_packedtags2.c \
 	yaffs2/yaffs_ecc.c \
 	yaffs2/yaffs_tagsvalidity.c
-LOCAL_CFLAGS =   -O2 -Wall -DCONFIG_YAFFS_UTIL -DCONFIG_YAFFS_DOES_ECC
+LOCAL_CFLAGS =   -Os -Wall -DCONFIG_YAFFS_UTIL -DCONFIG_YAFFS_DOES_ECC
 LOCAL_CFLAGS+=   -Wshadow -Wpointer-arith -Wwrite-strings -Wstrict-prototypes -Wmissing-declarations
 LOCAL_CFLAGS+=   -Wmissing-prototypes -Wredundant-decls -Wnested-externs -Winline
 LOCAL_CFLAGS+=   -DS_IWRITE=0200 -DS_IREAD=0400
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/yaffs2
-LOCAL_MODULE := mkyaffs2image
+LOCAL_MODULE := steam_mkyaffs2image
 include $(BUILD_EXECUTABLE)
 
 
@@ -43,31 +43,31 @@ LOCAL_SRC_FILES := \
 	yaffs2/yaffs_packedtags2.c \
 	yaffs2/yaffs_ecc.c \
 	yaffs2/yaffs_tagsvalidity.c
-LOCAL_CFLAGS =   -O2 -Wall -DCONFIG_YAFFS_UTIL -DCONFIG_YAFFS_DOES_ECC
+LOCAL_CFLAGS =   -Os -Wall -DCONFIG_YAFFS_UTIL -DCONFIG_YAFFS_DOES_ECC
 LOCAL_CFLAGS+=   -Wshadow -Wpointer-arith -Wwrite-strings -Wstrict-prototypes -Wmissing-declarations
 LOCAL_CFLAGS+=   -Wmissing-prototypes -Wredundant-decls -Wnested-externs -Winline
 LOCAL_CFLAGS+=   -DS_IWRITE=0200 -DS_IREAD=0400
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/yaffs2
-LOCAL_MODULE := libmkyaffs2image
-LOCAL_CFLAGS += -Dmain=mkyaffs2image_main
+LOCAL_MODULE := libsteam_mkyaffs2image
+LOCAL_CFLAGS += -Dmain=steam_mkyaffs2image_main
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libunyaffs
+LOCAL_MODULE := libsteam_unyaffs
 LOCAL_SRC_FILES := yaffs2/utils/unyaffs.c
-LOCAL_CFLAGS =   -O2 -Wall -DCONFIG_YAFFS_UTIL -DCONFIG_YAFFS_DOES_ECC
+LOCAL_CFLAGS =   -Os -Wall -DCONFIG_YAFFS_UTIL -DCONFIG_YAFFS_DOES_ECC
 LOCAL_CFLAGS+=   -Wshadow -Wpointer-arith -Wwrite-strings -Wstrict-prototypes -Wmissing-declarations
 LOCAL_CFLAGS+=   -Wmissing-prototypes -Wredundant-decls -Wnested-externs -Winline
 LOCAL_CFLAGS+=   -DS_IWRITE=0200 -DS_IREAD=0400
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/yaffs2
-LOCAL_CFLAGS += -Dmain=unyaffs_main
+LOCAL_CFLAGS += -Dmain=steam_unyaffs_main
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := unyaffs
+LOCAL_MODULE := steam_unyaffs
 LOCAL_SRC_FILES := yaffs2/utils/unyaffs.c
 LOCAL_MODULE_TAGS := eng
-LOCAL_CFLAGS =   -O2 -Wall -DCONFIG_YAFFS_UTIL -DCONFIG_YAFFS_DOES_ECC
+LOCAL_CFLAGS =   -Os -Wall -DCONFIG_YAFFS_UTIL -DCONFIG_YAFFS_DOES_ECC
 LOCAL_CFLAGS+=   -Wshadow -Wpointer-arith -Wwrite-strings -Wstrict-prototypes -Wmissing-declarations
 LOCAL_CFLAGS+=   -Wmissing-prototypes -Wredundant-decls -Wnested-externs -Winline
 LOCAL_CFLAGS+=   -DS_IWRITE=0200 -DS_IREAD=0400
@@ -75,10 +75,10 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/yaffs2
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := unyaffs
+LOCAL_MODULE := steam_unyaffs
 LOCAL_SRC_FILES := yaffs2/utils/unyaffs.c
 LOCAL_MODULE_TAGS := eng
-LOCAL_CFLAGS =   -O2 -Wall -DCONFIG_YAFFS_UTIL -DCONFIG_YAFFS_DOES_ECC
+LOCAL_CFLAGS =   -Os -Wall -DCONFIG_YAFFS_UTIL -DCONFIG_YAFFS_DOES_ECC
 LOCAL_CFLAGS+=   -Wshadow -Wpointer-arith -Wwrite-strings -Wstrict-prototypes -Wmissing-declarations
 LOCAL_CFLAGS+=   -Wmissing-prototypes -Wredundant-decls -Wnested-externs -Winline
 LOCAL_CFLAGS+=   -DS_IWRITE=0200 -DS_IREAD=0400
